ce947803fca5e965c4d7932ae73abbe8e803ca34
E-man <ebatis@outlook.com>
2016-11-14 18:33:24 +0100

    * Commit basic scene changes and canadarm hierarchy stuff AND script

---
+++ Assets/Scripts/MajorTom/Canadarm.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Canadarm : MonoBehaviour
+{
+    [SerializeField]
+    GameObject forearm;
+
+    [SerializeField]
+    GameObject upperArm;
+
+    [SerializeField]
+    GameObject grappler;
+
+    // Use this for initialization
+    void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
