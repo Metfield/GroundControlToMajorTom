9cbddc526871489a2daed88d6fe3a359dc7a8219
E-man <ebatis@outlook.com>
2016-11-17 14:24:37 +0100

    * Canadarm works with rigid bodies and collisions

--- Assets/Scripts/MajorTom/Canadarm.cs
+++ Assets/Scripts/MajorTom/Canadarm.cs
@@ -17,7 +17,7 @@ public class Canadarm : MonoBehaviour
 
     [SerializeField]
     GameObject grappler;
-
+    
     // Rotation speed
     [SerializeField]
     float rotationSpeed;
@@ -30,23 +30,48 @@ public class Canadarm : MonoBehaviour
     private float throttleValue;
     private float twistValue;
 
+    // Rigid bodies
+    Rigidbody upperArmRigidBody;
+    Rigidbody forearmRigidBody;    
+
     // Use this for initialization
     void Start ()
     {
         rotation = Vector3.zero;
-	}
+
+        // Get rigid component and relocate the center of mass to the local pivot in the upper arm
+        upperArmRigidBody = upperArmVertical.GetComponent<Rigidbody>();
+        upperArmRigidBody.centerOfMass = upperArmVertical.transform.localPosition;
+
+        // Do the same for the forearm
+        forearmRigidBody = forearm.GetComponent<Rigidbody>();
+        forearmRigidBody.centerOfMass = Vector3.zero;        
+    }
 	
-	// Update is called once per frame
-	void Update ()
+    void FixedUpdate()
     {
-        // Get all possible inputs
         GetInput();
 
-        // Handles all movement
-        upperArmHorizontal.transform.Rotate(horizontalValue * rotationSpeed, 0, 0, Space.Self);
-        upperArmVertical.transform.Rotate(0, verticalValue * rotationSpeed, 0, Space.Self);
-        forearm.transform.Rotate(0, throttleValue * rotationSpeed, 0, Space.Self);
-        grappler.transform.Rotate(twistValue, 0, 0, Space.Self);        
+        // Handle first joint (horizontal)
+        upperArmHorizontal.transform.Rotate(horizontalValue * (rotationSpeed * 0.05f), 0, 0, Space.Self);
+
+        // Handle second joint (upper arm horizontal)
+        Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, verticalValue * rotationSpeed, 0) * Time.deltaTime);
+        upperArmRigidBody.MoveRotation(upperArmRigidBody.rotation * deltaRotation);
+
+        // Handle forearm rotation
+        deltaRotation = Quaternion.Euler(new Vector3(0, throttleValue * rotationSpeed, 0) * Time.deltaTime);
+        forearmRigidBody.MoveRotation(forearmRigidBody.rotation * deltaRotation);
+
+        // Handle grapple rotation
+        grappler.transform.Rotate(twistValue, 0, 0, Space.Self);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {        
+        // Stop object if there is no input 
+        ClearBouncing();
     }
 
     // Write on all input members
@@ -56,5 +81,20 @@ public class Canadarm : MonoBehaviour
         verticalValue = Input.GetAxis("Vertical");
         throttleValue = Input.GetAxis("ThrottleStick");
         twistValue = Input.GetAxis("TwistStick");
+    }
+
+    void ClearBouncing()
+    {
+        if((horizontalValue + verticalValue == 0))
+        {
+            upperArmRigidBody.velocity = Vector3.zero;
+            upperArmRigidBody.angularVelocity = Vector3.zero;
+        }
+
+        if(throttleValue == 0)
+        {
+            forearmRigidBody.velocity = Vector3.zero;
+            forearmRigidBody.angularVelocity = Vector3.zero;
+        }
     }
 }
