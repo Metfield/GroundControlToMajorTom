099ec7dce1127e0e6bb00a6f16698652371f851c
E-man <ebatis@outlook.com>
2016-11-22 09:52:56 +0100

    * Added SpaceX Dragon Cargo shuttle model * Some scripts * Collision boxes for ISS and Canadarm * changed some prefabs

---
+++ Assets/Scripts/MajorTom/CargoShuttle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CargoShuttle : MonoBehaviour
+{
+    private Rigidbody cargoRigidBody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cargoRigidBody = GetComponent<Rigidbody>();
+
+        cargoRigidBody.centerOfMass = transform.localPosition;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+
+      
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("NOOOOOOOOOO!!!");
+    }
+}
---
+++ Assets/Scripts/MajorTom/GrappleFixture.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrappleFixture : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+   //S ADDD COOLIIISIOOOON TRIIIGGEEEEEEEEEEEERS
+}
